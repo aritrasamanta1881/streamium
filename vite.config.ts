4d67e04b57e081eecd186c3f0f440aa057f8bbf9
aritrasamanta1881 <banasrisamanta463@gmail.com>
2026-06-15 11:29:07 +0530

    Update vite.config.ts
    Change: PWA configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,22 +10,19 @@ export default defineConfig({
       manifest: {
         name: "Streamium",
         short_name: "Streamium",
-        description: "Stream anytime, anywhere!!",
+        description: "Stream anything, anytime, anywhere, for free!!",
         theme_color: "#111827",
         background_color: "#111827",
         display: "fullscreen",
         start_url: "/",
         icons: [
           {
-            src: "favicon.png",
-            sizes: "192x192",
+            src: "/favicon.png",
+            sizes: "192x192 512×512",
             type: "image/png"
+            purpose: "any maskable"
           },
-          {
-            src: "favicon.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
+          
         ]
       }
     })
