5883bf179493515c7bcfe124a180c629e3ccb141
aritrasamanta1881 <banasrisamanta463@gmail.com>
2026-06-16 07:14:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,25 +5,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [
     sveltekit(),
-    SvelteKitPWA({
-      registerType: "autoUpdate",
-      strategies: "generateSW", //
-      srcDir: 'src',   //
-      scope: '/',     //
-      manifest: {
-        name: "Streamium",
-        short_name: "Streamium",
-        description: "Stream anything, anytime, anywhere, for free!!",
-        theme_color: "#111827",
-        background_color: "#111827",
-        display: "fullscreen",
-        start_url: "/",
-        icons: [
-          {
-            src: "/favicon.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
           
         ]
       }
