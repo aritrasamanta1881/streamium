e86b83703efc1971454e94c87272bb21127a4eef
aritrasamanta1881 <banasrisamanta463@gmail.com>
2026-06-16 07:50:38 +0530

    Update sw.js
    Made a optional for PWA

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,16 @@
-self.addEventListener('fetch', function(event) {
-  // 
+const CACHE_NAME = 'streamium-v1';
+
+// Install event (Prepares the app in the background)
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+// Fetch event (Required by Chrome to enable PWA installation)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
 
